699ca412ce73ce7585ffe7e6a6f66533c82b221e
Samer78 <samer.nuwwareh@gmail.com>
2015-01-21 20:42:35 -0500

    R_programming_Assignment 2
    In this version, I entered my functions on the forked file. I deleted
    some comments from the original file, and these are shown in red. My
    comments and functions lines are coloured in green.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following two functions can be used to cache the inverse of a matrix
 
-## Write a short comment describing this function
+## The first function is "makeCacheMatrix". 
+### This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                m <- NULL
+                set <- function(y){
+                                x<<-y
+                                m<<-NULL
+                }
+                get <- function() x
+                setmatrix <- function(solve) m <<- solve
+                getmatrix <- function() m
+                list(set=set, get=get,
+                     setmatrix=setmatrix,
+                     getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## The second function computes the inverse of the special 
+###"matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                m<-x$getmatrix()
+                if(!is.null(m)){
+                                message("getting cached data")
+                                return(m)
+                }
+                matrix<-x$get()
+                m<-solve(matrix, ...)
+                x$setmatrix(m)
+                m
 }
+
+### The following is a test for the two functions:
+
+test <- makeCacheMatrix() # assign the first function to a variable called "test"
+
+test$set(matrix(1:4, 2, 2)) # assign (or set) the matrix to be sovled
+
+cacheSolve(test) # return the inverse of the test matrix
